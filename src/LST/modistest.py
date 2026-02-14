32246e13c89395a8f74c839a37d082cb5ce90d3d
David D.Kovacs <david.kovacs@geo.tuwien.ac.at>
2026-02-14 08:48:17 +0100

    Renamed vars

--- src/LST/modistest.py
+++ src/LST/modistest.py
@@ -1,26 +1,27 @@
 import rioxarray as rxr
 from osgeo import gdal
 import xarray as xr
+from typing import List
 import rasterio
 from pyhdf.SD import SD, SDC
 import matplotlib.pyplot as plt
 import numpy as np
-path = ("/home/ddkovacs/shares/climers/Projects/CCIplus_Soil_Moisture/07_data/LPRM/07_debug/"
-        "daytime_retrieval/LST/MODIS/midwest/lst/MYD11_L2.A2018003.1930.061.2021316030624.hdf")
+# path = ("/home/ddkovacs/shares/climers/Projects/CCIplus_Soil_Moisture/07_data/LPRM/07_debug/"
+#         "daytime_retrieval/LST/MODIS/midwest/lst/MYD11_L2.A2018003.1930.061.2021316030624.hdf")
+path_sr = "/home/ddkovacs/Desktop/MYD09.A2018365.2010.061.2021348142533.hdf"
 
-
-def open_hdf(path, var):
+def open_hdf(path, var: List):
 
     data = SD(path)
-    LST_var = data.select(var)
-    _LST_array = LST_var[:].astype(np.float64)
-    LST_attrs = LST_var.attributes()
-    LST_scale = LST_attrs["scale_factor"]
-    LST_offset = LST_attrs["add_offset"]
-    LST_fillvalue = LST_attrs['_FillValue']
+    SDvar = data.select(var)
+    SD_var_array = SDvar[:].astype(np.float64)
+    attrs = SDvar.attributes()
+    scale = attrs["scale_factor"]
+    offset = attrs["add_offset"]
+    fillvalue = attrs['_FillValue']
 
-    LST_array = np.where(_LST_array == LST_fillvalue,np.nan,_LST_array)
-    LST_array_calibrated = (LST_array + LST_offset ) * LST_scale
+    valid_array = np.where(SD_var_array == fillvalue,np.nan,SD_var_array)
+    scaled_valid_array = (valid_array + offset ) * scale
 
     lat_var = data.select("Latitude")
     lat_array = lat_var[:].astype(np.float64)
@@ -28,7 +29,7 @@ def open_hdf(path, var):
     lon_var = data.select("Longitude")
     lon_array = lon_var[:].astype(np.float64)
 
-    return {"data":LST_array_calibrated, "lat": lat_array, "lon" : lon_array}
+    return {"data":scaled_valid_array, "lat": lat_array, "lon" : lon_array}
 
 
 def pad_array(array): # TODO: maybe needs to edit the shape
@@ -54,9 +55,12 @@ def xr_from_arrays(data,lat,lon):
     return dataset
 
 
-data_dict = open_hdf(path, "LST")
-data_dict["data"] = pad_array(data_dict["data"])
-ds_LST = xr_from_arrays(data_dict["data"], data_dict["lat"], data_dict["lon"])
+SR_dict = open_hdf(path_sr, '1km Surface Reflectance Band 5')
+SR_dict["data"] = pad_array(SR_dict["data"])
+ds_SR = xr_from_arrays(SR_dict["data"], SR_dict["lat"], SR_dict["lon"])
+
+
+
 
 plt.figure()
 ds_LST["LST"].plot.pcolormesh(x= "lon", y= "lat")
