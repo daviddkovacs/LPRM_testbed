61acf4d0e54af87e834c789f176764787bd1e998
David D.Kovacs <david.kovacs@geo.tuwien.ac.at>
2026-02-14 08:32:55 +0100

    Reader functions (MODIS)

---
+++ src/config/paths.py
@@ -0,0 +1,15 @@
+path_bt = "/home/ddkovacs/shares/climers/Projects/CCIplus_Soil_Moisture/07_data/LPRM/01_resampled_bt/"
+path_lprm = "/home/ddkovacs/shares/climers/Projects/CCIplus_Soil_Moisture/07_data/LPRM/04_retrieved/coarse_resolution/"
+path_aux = "/home/ddkovacs/shares/climers/Projects/CCIplus_Soil_Moisture/07_data/LPRM/02_aux_data/coarse_resolution/lprm_v6/soil_content"
+
+# ISMN intercomparison
+#Satellite stack with T_s and T_c and BTs
+sat_stack_path_day = "/home/ddkovacs/shares/climers/Projects/CCIplus_Soil_Moisture/07_data/LPRM/debug/daytime_retrieval/satellite_data/US_2024_day.nc"
+sat_stack_path_night = "/home/ddkovacs/shares/climers/Projects/CCIplus_Soil_Moisture/07_data/LPRM/debug/daytime_retrieval/satellite_data/US_2024_night.nc"
+ismn_data_path = '/home/ddkovacs/shares/climers/Projects/CCIplus_Soil_Moisture/07_data/LPRM/debug/daytime_retrieval/ismn_data/SCAN.zip'
+
+# S3 paths
+S3_SLSTR_path = "/home/ddkovacs/shares/climers/Projects/CCIplus_Soil_Moisture/07_data/LPRM/07_debug/daytime_retrieval/LST/S3_SLSTR"
+
+# MODIS paths
+MODIS_path = "/home/ddkovacs/shares/climers/Projects/CCIplus_Soil_Moisture/07_data/LPRM/07_debug/daytime_retrieval/LST/MODIS/"
